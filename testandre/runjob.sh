d482385eb84d334c0b40dbd5684b000e62b66b59
Andre Hacker <andrephacker@gmail.com>
2013-03-12 17:43:07 +0100

    minor

--- testandre/runjob.sh
+++ testandre/runjob.sh
@@ -9,6 +9,7 @@ ${HADOOP_PREFIX}/bin/hadoop fs -mkdir testandre/input
 # Copy input to hadoop
 ${HADOOP_PREFIX}/bin/hadoop fs -put input/* testandre/input
 
+# Run task
 # hadoop jar <jarfile> [Main class] args...
 # For our tasks, the args are input and output directory
 ${HADOOP_PREFIX}/bin/hadoop jar testandre.jar de.andrehacker.TestAndre testandre/input testandre/output
