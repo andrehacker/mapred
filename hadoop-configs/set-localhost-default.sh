0a9078ed14d453b87be917f468d4b2c3a2403d8a
Andre Hacker <andrephacker@gmail.com>
2013-04-04 23:44:14 +0200

    config scripts

---
+++ hadoop-configs/set-localhost-default.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#set -x verbose
+
+if [ "$HADOOP_PREFIX" = "" ]; then
+    echo "Error: JAVA_HOME is not set."
+    exit 1  # 0 = success, non-zero is failure
+fi
+echo "copy config files to hadoop conf dir"
+cp config-localhost/* $HADOOP_PREFIX/conf
+echo "done"
