3d5b02fe03b351a48b2f1814006e78abab34813b
Andre Hacker <andrephacker@gmail.com>
2013-04-30 18:28:24 +0200

    minor

--- tpc-h/generate-data.sh
+++ tpc-h/generate-data.sh
@@ -3,4 +3,4 @@
 cd ~/dev/tpc-h/tpch_2_15.0/dbgen
 
 # Last parameter is scale factor (GB): 1, 10, 30, 100, 300, 1000, ...
-./dbgen -T o -s 10
+./dbgen -T o -s 1
--- tpc-h/runjob-hdfs.sh
+++ tpc-h/runjob-hdfs.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 
+# 8-10 min to load data to hdfs on desktop 
+
 DIR=`dirname "$0"`
 DIR=`cd "$bin"; pwd`
 cd $DIR
 
-INPUT_DIR=~/dev/tpc-h/tpch_2_15.0/dbgen/
+INPUT_DIR=~/dev/tpc-h/
+SCALE_FACTOR=1
+SKIP_HDFS_COPY=1
 #HDFS_NAMENODE=master:9000
-HDFS_NAMENODE=localhost:9000 # if running dfs and stratosphere in local(host) mode
+HDFS_NAMENODE=desktop:9000 # if running dfs and stratosphere in local(host) mode
 HDFS_PREFIX=/user/andre
 HDFS_DIR=tpch-test
 JAR_PATH=${STRATOSPHERE_HOME}/examples/pact/pact-examples-0.2-TPCHQuery3.jar
 
+
 # Run task
 set -x verbose
 
+if [SKIP_HDFS_COPY -eq 1]; then
 # Clean up
-${HADOOP_PREFIX}/bin/hadoop fs -rmr $HDFS_DIR
+    ${HADOOP_PREFIX}/bin/hadoop fs -rmr $HDFS_DIR
 
 # Copy input to hadoop
-${HADOOP_PREFIX}/bin/hadoop fs -mkdir $HDFS_DIR/input
-${HADOOP_PREFIX}/bin/hadoop fs -put $INPUT_DIR/*.tbl $HDFS_DIR/input
+    ${HADOOP_PREFIX}/bin/hadoop fs -mkdir $HDFS_DIR/input
+    ${HADOOP_PREFIX}/bin/hadoop fs -put $INPUT_DIR/*-${SCALE_FACTOR}.tbl $HDFS_DIR/input
+fi;
 
 ARG1=4    # noSubTasks
-ARG2=hdfs://${HDFS_NAMENODE}${HDFS_PREFIX}/${HDFS_DIR}/input/orders.tbl
-ARG3=hdfs://${HDFS_NAMENODE}${HDFS_PREFIX}/${HDFS_DIR}/input/lineitem.tbl
+ARG2=hdfs://${HDFS_NAMENODE}${HDFS_PREFIX}/${HDFS_DIR}/input/orders-${SCALE_FACTOR}.tbl
+ARG3=hdfs://${HDFS_NAMENODE}${HDFS_PREFIX}/${HDFS_DIR}/input/lineitem-${SCALE_FACTOR}.tbl
 ARG4=hdfs://${HDFS_NAMENODE}${HDFS_PREFIX}/${HDFS_DIR}/output
 $STRATOSPHERE_HOME/bin/pact-client.sh run -w -j $JAR_PATH -a $ARG1 $ARG2 $ARG3 $ARG4
 
