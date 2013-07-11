0fdb993880ec8d0def6af46df5487408f94a9fbc
Andre Hacker <andrephacker@gmail.com>
2013-07-11 11:08:41 +0200

    moved config stuff to other repo

--- config-system/passwordless-ssh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# from http://hortonworks.com/kb/generating-ssh-keys-for-passwordless-login/
-
-# generate the public private keys
-ssh-keygen -t dsa -P '' -f ~/.ssh/id_dsa
-
-#authorize the key by adding it to the list of authorized keys
-cat ~/.ssh/id_dsa.pub >> ~/.ssh/authorized_keys
